fe8215fca7e1df94f9c9d53998de45ae99ef2bef
Johnathan Bergman <jbergman@bsd220.org>
2021-03-09 10:16:13 -0600

    Made dev branch

--- 'Za Hunter/ContentView.swift
+++ 'Za Hunter/ContentView.swift
@@ -6,11 +6,21 @@
 //
 
 import SwiftUI
+import MapKit
+
+@State private var region = MKCoordinateRegion(
+    center: CLLocationCoordinate2D(
+        latitude: 42.15704,
+        longitude: -88.14812),
+    span: MKCoordinateSpan(
+        latitudeDelta: 0.05,
+        longitudeDelta: 0.05)
+)
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        Map(coordinateRegion: $region)
+        
     }
 }
 
@@ -19,3 +29,4 @@ struct ContentView_Previews: PreviewProvider {
         ContentView()
     }
 }
+
